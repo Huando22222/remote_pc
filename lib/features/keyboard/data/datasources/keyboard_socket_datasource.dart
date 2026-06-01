6cc0e8f82ccb17e68137f66dcfef58aac35b95bd
HuanDo22222 <huando22222@gmail.com>
2026-06-02 00:15:49 +0700

    keyboard symbol

--- lib/features/keyboard/data/datasources/keyboard_socket_datasource.dart
+++ lib/features/keyboard/data/datasources/keyboard_socket_datasource.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pc_remote/core/constants/socket_constants.dart';
@@ -15,14 +16,18 @@ class KeyboardSocketDatasource {
   final SocketClient socketClient;
 
   Future<void> sendText(String text) async {
+    log('emit keyboard_text "$text"', name: 'KeyboardSocketDatasource');
     socketClient.emit(SocketConstants.eventKeyboardText, {'text': text});
   }
 
   Future<void> sendKey(String key) async {
+    log('emit keyboard_key $key', name: 'KeyboardSocketDatasource');
     socketClient.emit(SocketConstants.eventKeyboardKey, {'key': key});
   }
 
   Future<void> sendShortcut(List<String> keys) async {
+    log('emit keyboard_shortcut ${keys.join('+')}',
+        name: 'KeyboardSocketDatasource');
     socketClient.emit(SocketConstants.eventKeyboardShortcut, {'keys': keys});
   }
 }
