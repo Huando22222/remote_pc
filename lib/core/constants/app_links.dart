c4a77155a846114ec1af3f2f4fffd14a4151d4f0
HuanDo22222 <huando22222@gmail.com>
2026-06-04 17:14:18 +0700

    link

--- lib/core/constants/app_links.dart
+++ lib/core/constants/app_links.dart
@@ -2,5 +2,5 @@ class AppLinks {
   AppLinks._();
 
   static const desktopServerDownload =
-      'https://drive.google.com/drive/folders/YOUR_FOLDER_ID';
+      'https://drive.google.com/drive/folders/1fEkxanyMA702R8izwXu7gQCnSCMSU9g6?usp=sharing';
 }
